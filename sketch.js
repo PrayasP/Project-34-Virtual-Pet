10a8b08de0d042e92a8b5d758ea2b98a7584e4bf
PrayasP <74405983+PrayasP@users.noreply.github.com>
2021-08-29 17:06:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+var dog, happyDog, database, foodS, foodStock;
+
+function preload()
+{
+	dog = loadImage("images/dogImg.png");
+  happyDog = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500,500);
+  database = firebase.database();
+
+  dogSprite = createSprite(250,250);
+  dogSprite.addImage(dog);
+  dogSprite.scale = 0.24;
+
+  
+  foodStock = database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  
+}
+
+
+function draw() {  
+
+  background(46, 139, 87);
+
+  
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dogSprite.addImage(happyDog);
+  }
+
+
+  drawSprites();
+  
+  fill("BLACK");
+  text("Food remaining: "+foodS,170,200);
+  textSize(18);
+  text("Note : Press UP ARROW to feed the pet", 130,10,300,20);
+
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+if(x<= 0){
+  x =0;
+}else{
+  x = x-1;
+}
+
+database.ref('/').update({
+  Food: x,
+})
+}
+
+
+
+
